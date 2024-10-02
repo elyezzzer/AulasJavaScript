57d0333e9f466909c4f01b0fa6017b8e2150fb97
elyezzzer <156719497+elyezzzer@users.noreply.github.com>
2024-10-01 21:50:54 -0300

    Add files via upload

---
+++ JavaScript/script.js
@@ -0,0 +1,28 @@
+var time = "Vasco da Gama";
+let idade = 25;
+idade = "25 anos";
+const cidade = "São Paulo";
+
+//Primitivos
+let saudacao = "Olá mundo";
+let preco = 19.99;
+let estaChovendo = false;
+
+//Complexos
+let teclado = {
+    marca: 'HP',
+    peso: '1kg',
+    cor: 'Branco',
+    tipo: 'Mecanico'
+}
+
+teclado.marca = 'Dell';
+console.log(teclado);
+
+let cores = ["Vermelho", "Azul", "Laranja"];
+
+//concatenacao entre strings
+
+let nome = "Elyezer";
+let message = "Olá " + nome + " seja bem vindo!";
+console.log(message);
