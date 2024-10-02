f4201d7068d374d0f044b5157cbba31faf3cb875
elyezzer <elyezzer1247@gmail.com>
2024-10-02 20:31:37 -0300

    feature/Comentários

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,3 +1,4 @@
+//Exercicio 01
 /*let idade = prompt("Digite sua idade: ")
 if (idade>=18){
     console.log("Você é de maior");
@@ -5,6 +6,7 @@ if (idade>=18){
     console.log("Você é de menor");
 } */
 
+//Exercicio 02
 /*let numero = prompt("Digite um número:")
 if (numero % 2 == 0){
     console.log("Par");
@@ -12,6 +14,7 @@ if (numero % 2 == 0){
     console.log("Impar");
 } */
 
+//Exercicio 03
 /*let numero = prompt("Digite um número: ")
 if (numero != 0){
     if (numero >0){
@@ -23,6 +26,7 @@ if (numero != 0){
     console.log("Zero");
 } */
 
+//Exercicio 04
 let tabuada = prompt("Digite o número da tabuada que quer ver: ");
 for (let i=1; i<11; i++){
     let resultado=tabuada*i;
