1153bbfa9ff92c5fec8693fadf4162e532455e36
elyezzer <elyezzer1247@gmail.com>
2024-10-02 19:53:11 -0300

    aula03

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,36 +1,40 @@
-var time = "Vasco da Gama";
-let idade = 25;
-idade = "25 anos";
-const cidade = "São Paulo";
+//operadores aritimeticos
+let x = 10;
+let y = 5;
 
-//Primitivos
-let saudacao = "Olá mundo";
-let preco = 19.99;
-let estaChovendo = false;
+let soma = x + y;
+let subtracao = x - y;
+let multiplicacao = x * y;
+let divisao = x / y;
+let resto = x % y;
 
-//Complexos
-let teclado = {
-    marca: 'HP',
-    peso: '1kg',
-    cor: 'Branco',
-    tipo: 'Mecanico'
+console.log("A soma é: ",soma);
+
+//operadores de atribuicao
+let a = 10;
+let somaX = a + 5;
+a += 5;
+
+//operadores de comparacao
+let time = "Santos";
+let maiorTime = time == "Santos";
+console.log(time);
+
+//estruturas condicionais
+const idade = 18;
+if (idade>=18){
+    console.log("Você é de maior.");
+} else {
+    console.log("Você é de menor");
 }
 
-teclado.marca = 'Dell';
-console.log(teclado);
+//estruturas de repeticao
+for (let i=1;i < 6;i++){
+    let total = 5 * i;
+    console.log("5 x "+ i +"="+ total);
+}
 
-let cores = ["Vermelho", "Azul", "Laranja"];
-
-//concatenacao entre strings
-
-let nome = "Elyezer";
-let message = "Olá " + nome + " seja bem vindo!";
-console.log(message);
-
-//conversao de tipos
-
-let numero = 100;
-let numeroConvertido = String(numero);
-
-let str = "123";
-let strConvertido = Number(str);
+let contador = 10;
+while (contador <= 10){
+    
+}
