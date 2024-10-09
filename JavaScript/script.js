055ec52b6986a958b714e056e127e6f0eec2ab69
elyezzer <elyezzer1247@gmail.com>
2024-10-08 22:57:21 -0300

    Versão com funções separadas para cada operação

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,20 +1,33 @@
 function calculadora(n1,n2,operacao){
     if(operacao == "soma"){
-        return n1+n2;
-    } 
+        return soma(n1,n2)
+    }
     if(operacao == "subtracao"){
-        return n1-n2;
+        return subtracao(n1,n2)
     }
     if(operacao == "multiplicacao"){
-        return n1*n2;
+        return multiplicacao(n1,n2)
     }
     if(operacao == "divisao"){
-        return n1/n2;
+        return divisao(n1,n2)
     }
-    return "Operação inválida."
+    return "Operação Inválida";
 }
-console.log(calculadora(10,20,"subtracao"));
-console.log(calculadora(10,20,"soma"));
-console.log(calculadora(10,20,"multiplicacao"));
-console.log(calculadora(10,20,"divisao"));
-console.log(calculadora(10,20,"erro"));
+function soma(n1,n2){
+    return n1+n2;
+}
+function subtracao(n1,n2){
+    return n1-n2;
+}
+function multiplicacao(n1,n2){
+    return n1*n2;
+}
+function divisao(n1,n2){
+    return n1/n2;
+}
+
+console.log(calculadora(10,10,"erro"));
+console.log(calculadora(10,10,"soma"));
+console.log(calculadora(10,10,"subtracao"));
+console.log(calculadora(10,10,"multiplicacao"));
+console.log(calculadora(10,10,"divisao"));
