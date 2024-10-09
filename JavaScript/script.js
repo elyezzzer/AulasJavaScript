696267c470a66ad5643aaf64cda9170bb68be42f
elyezzer <elyezzer1247@gmail.com>
2024-10-08 22:45:21 -0300

    Calculadora-files added

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,31 +1,20 @@
-// Função simples
-function exibirBoasVindas(){
-    return console.log("Olá, seja bem vindo!");
+function calculadora(n1,n2,operacao){
+    if(operacao == "soma"){
+        return n1+n2;
+    } 
+    if(operacao == "subtracao"){
+        return n1-n2;
+    }
+    if(operacao == "multiplicacao"){
+        return n1*n2;
+    }
+    if(operacao == "divisao"){
+        return n1/n2;
+    }
+    return "Operação inválida."
 }
-exibirBoasVindas();
-
-//Função com parâmetros
-function soma(a, b, c){
-    const total = a + b + c;
-    return console.log("Total soma: ", total);
-}
-soma(10,20,30);
-
-//Função anônima
-const multiplicar = function(x, y){
-    return console.log("Multiplicação: ", x * y);
-}
-multiplicar(3,5);
-
-//Função arrow function
-const dividir = (a, b) => a / b;
-console.log("Divisão: ", dividir(10,2));
-
-//Função de callback
-function inicializarProjeto(){
-    exibirBoasVindas();
-    soma(40, 50, 10);
-    multiplicar(10,6);
-    dividir(20,2);
-}
-inicializarProjeto();
+console.log(calculadora(10,20,"subtracao"));
+console.log(calculadora(10,20,"soma"));
+console.log(calculadora(10,20,"multiplicacao"));
+console.log(calculadora(10,20,"divisao"));
+console.log(calculadora(10,20,"erro"));
